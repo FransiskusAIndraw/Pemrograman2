d368012246d54cdfbf6139c77e9d6abe131643bd
FransiskusAIndraw <2210817210032@mhs.ulm.ac.id>
2023-09-13 16:50:10 +0800

    TO BE LAPRAK 1

---
+++ PRAKTIKUM1/src/PRAK001_FRANSISKUSASSISIINDRAWIJAYA_2210817210032.java
@@ -0,0 +1,5 @@
+public class PRAK001_FRANSISKUSASSISIINDRAWIJAYA_2210817210032 {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ PRAKTIKUM1/src/PRAK002.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PRAK002 {
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Masukkan Makanan Favorit:  ");
+        String Mak_fav = inp.nextLine();
+        System.out.println("makanan Favorit "+ Mak_fav);
+
+    }
+}
