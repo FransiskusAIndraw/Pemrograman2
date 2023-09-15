d3a697e1f471edbc71f67d8ca788938fe05eed35
FransiskusAIndraw <2210817210032@mhs.ulm.ac.id>
2023-09-15 10:05:33 +0800

    TO BE LAPRAK 1

--- PRAKTIKUM1/src/PRAK001_2210817210032_FransiskusAssisiIndraWIjaya.java
+++
@@ -1,7 +0,0 @@
-public class PRAK001_2210817210032_FransiskusAssisiIndraWIjaya {
-    public static void main(String[]args){
-        System.out.println("Selamat Pagi, Fransiskus Assisi Indra WIjaya");
-        System.out.println("Selamat Siang, Fransiskus Assisi Indra WIjaya");
-        System.out.println("Selamat Sore, Fransiskus Assisi Indra WIjaya");
-    }
-}
