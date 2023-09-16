b5929bf8301ff80d8fa78cbf36997b4f1796c9c9
FransiskusAIndraw <115336850+FransiskusAIndraw@users.noreply.github.com>
2023-09-16 10:46:09 +0800

    Add files via upload

---
+++ PRAKTIKUM1/PRAK002_2210817210032_FransiskusAssisiIndraWijaya.java
@@ -0,0 +1,15 @@
+
+public class PRAK002_2210817210032_FransiskusAssisiIndraWijaya {
+    public static void main(String[] args){
+        System.out.println("#############################################");
+        System.out.println("#                                           #");
+        System.out.println("#       Fransiskus assisi indra wijaya      #");
+        System.out.println("#                                           #");
+        System.out.println("#                2210817210032              #");
+        System.out.println("#                                           #");
+        System.out.println("#############################################");
+
+
+
+    }
+}
