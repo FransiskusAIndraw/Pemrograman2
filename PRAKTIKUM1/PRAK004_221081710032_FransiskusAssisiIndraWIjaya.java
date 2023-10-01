c5e3587c6fbd09746c2e3d136fcb02691ee6f4af
FransiskusAIndraw <115336850+FransiskusAIndraw@users.noreply.github.com>
2023-10-01 14:06:02 +0800

    Delete PRAKTIKUM1 directory

--- PRAKTIKUM1/PRAK004_221081710032_FransiskusAssisiIndraWIjaya.java
+++
@@ -1,7 +0,0 @@
-public class PRAK004_221081710032_FransiskusAssisiIndraWIjaya {
-    public static void main(String[] args) {
-        String nama = "Fransiskus Assisi Indra Wijaya";
-        int umur = 20;
-        System.out.println("Perkenalkan, Nama Saya, "+nama +" Umur Saya "+umur);
-    }
-}
